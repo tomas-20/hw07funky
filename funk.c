4a7fd18ebe704723b95110ae056575ded00bc345
tomas-20 <tacuna20@stuy.edu>
2021-10-06 00:27:46 -0400

    made a print_array(int[]) funtion to, you guessed it, print arrays

--- funk.c
+++ funk.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+void print_array(int array[]) {
+  printf("[ ");
+  for (int i = 0; i < sizeof *array; i ++) {
+    printf("%d ", array[i]);
+  }
+  printf("]\n");
+}
+int main() {
+  int bob[4];
+  bob[0] = 2;
+  bob[1] = 1;
+  bob[2] = 8;
+  bob[3] = 7;
+  print_array(bob);
+  return 0;
+}
