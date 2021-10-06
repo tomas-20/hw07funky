94d71213e8df2d252c8a2e55ac62a4436386e422
tomas-20 <tacuna20@stuy.edu>
2021-10-06 00:38:53 -0400

    gave in to the badness and added a size variable

--- funk.c
+++ funk.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-void print_array(int array[]) {
+void print_array(int array[], int size) {
   printf("[ ");
-  for (int i = 0; i < sizeof *array; i ++) {
+  for (int i = 0; i < size; i ++) {
     printf("%d ", array[i]);
   }
   printf("]\n");
 }
 int main() {
-  int bob[4];
+  int bob[5];
   bob[0] = 2;
   bob[1] = 1;
   bob[2] = 8;
   bob[3] = 7;
-  print_array(bob);
+  bob[4] = 9;
+  print_array(bob, 5);
   return 0;
 }
